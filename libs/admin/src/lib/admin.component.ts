7073687ac58328586ad7b878372bda46c144bc89
hugo <hugo.herrador@gmail.com>
2022-12-13 20:51:36 +0100

    Create new lib and main comp of the lib

---
+++ libs/admin/src/lib/admin.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portal-map-nx-ngrx-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css'],
+})
+export class AdminComponent {}
