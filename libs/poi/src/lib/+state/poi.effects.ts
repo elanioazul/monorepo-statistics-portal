cf21180e25b12b9e48c2619314f9103544e43737
hugo <hugo.herrador@gmail.com>
2022-12-26 16:43:16 +0100

    Create a new effect that listen to visitPoi actions and and increase the stat of the poi visited by interacting with localStorage

--- libs/poi/src/lib/+state/poi.effects.ts
+++ libs/poi/src/lib/+state/poi.effects.ts
@@ -4,9 +4,11 @@ import { fetch } from '@nrwl/angular';
 
 import { map } from 'rxjs/operators';
 import { PoiService } from '../poi.service';
+import { LocalStorageService } from '../local-storage.service';
 
 import * as PoiActions from './poi.actions';
 import * as PoiFeature from './poi.reducer';
+import { EMPTY } from 'rxjs';
 
 @Injectable()
 export class PoiEffects {
@@ -30,7 +32,21 @@ export class PoiEffects {
     )
   );
 
-  constructor(private poiService: PoiService) {
+  visit$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(PoiActions.visitPoi),
+      fetch({
+        run: (action) => {
+          const stat = this.localstorageServ.getData('tour-' + action.poiId);
+          const total = stat ? Number(stat) + 1 : 1;
+          this.localstorageServ.saveData('tour-' + action.poiId, total.toString());
+          return EMPTY;
+        }
+      })
+    )
+  );
+
+  constructor(private poiService: PoiService, private localstorageServ: LocalStorageService) {
 
   }
 }
