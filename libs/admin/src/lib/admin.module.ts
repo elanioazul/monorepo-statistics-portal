3224c634b614a880ca31ac0c218b7ea09c2045e1
hugo <hugo.herrador@gmail.com>
2022-12-26 20:15:22 +0100

    Install dep needed to show chart at admin portal

--- libs/admin/src/lib/admin.module.ts
+++ libs/admin/src/lib/admin.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
 import { RouterModule } from '@angular/router';
-
+import { PoiModule } from '@portal-map-nx-ngrx/poi';
+import { ChartModule } from 'primeng/chart';
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild([
       { path: '', component: AdminComponent }
-    ])
+    ]),
+    PoiModule,
+    ChartModule
   ],
   declarations: [AdminComponent],
 
